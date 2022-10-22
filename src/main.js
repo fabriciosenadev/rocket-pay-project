8b32b6a77b6023766ef64c2da9ee16b8514f648d
Fabricio Sena <fabricior.sena@outlook.com>
2022-10-21 22:53:43 -0300

    add finished project

--- src/main.js
+++ src/main.js
@@ -1,1 +1,221 @@
 import "./css/index.css"
+import IMask from "imask"
+
+const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
+const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
+const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
+
+// #region control dynamically color to card
+function setCardType(type) {
+  const colors = {
+    visa: ["#436D99", "#2D57F2"],
+    mastercard: ["#DF6F29", "#C69347"],
+    default: ["black", "gray"],
+  }
+
+  ccBgColor01.setAttribute("fill", colors[type][0])
+  ccBgColor02.setAttribute("fill", colors[type][1])
+  ccLogo.setAttribute("src", `cc-${type}.svg`)
+}
+//setCardType("visa")
+
+// how to allow to use with global access a local function
+globalThis.setCardType = setCardType
+
+// #endregion
+
+//#region mask control
+const securityCode = document.querySelector("#security-code")
+const securityCodePattern = {
+  mask: "0000",
+}
+const securityCodeMasked = IMask(securityCode, securityCodePattern)
+
+const expirationDate = document.querySelector("#expiration-date")
+const expirationDatePattern = {
+  mask: "MM{/}YY",
+  blocks: {
+    YY: {
+      mask: IMask.MaskedRange,
+      from: String(new Date().getFullYear()).slice(2),
+      to: String(new Date().getFullYear() + 10).slice(2),
+    },
+    MM: {
+      mask: IMask.MaskedRange,
+      from: 1,
+      to: 12,
+    },
+  },
+}
+
+const expirationDateMasked = IMask(expirationDate, expirationDatePattern)
+
+const cardNumber = document.querySelector("#card-number")
+const cardNumberPattern = {
+  mask: [
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^4\d{0,15}/,
+      cardtype: "visa",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}/,
+      cardtype: "mastercard",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      cardtype: "default",
+    },
+  ],
+  dispatch: function (appended, dynamicMasked) {
+    // appended -> get any key pressed
+    // dynamicMasked -> get mask dynamic created to be used
+
+    //doing replace by key got to allow only digits, replacind letters by space
+    const number = (dynamicMasked.value + appended).replace(/\D/g, "")
+
+    // prettier written: (regex) => number.match(regex)
+    const foundMask = dynamicMasked.compiledMasks.find(function (item) {
+      return number.match(item.regex)
+    })
+
+    // console.log(foundMask)
+    return foundMask
+  },
+}
+
+const cardNumberMasked = IMask(cardNumber, cardNumberPattern)
+//#endregion
+
+//#region add card name
+const addButton = document.querySelector("#add-card")
+addButton.addEventListener("click", () => {
+  alert("Cartão adicionado")
+})
+
+// get by tag name
+document.querySelector("form").addEventListener("submit", (event) => {
+  // stop reload by submit
+  event.preventDefault()
+})
+
+const cardHolder = document.querySelector("#card-holder")
+cardHolder.addEventListener("input", (event) => {
+  const ccHolder = document.querySelector(".cc-holder .value")
+
+  ccHolder.innerHTML =
+    cardHolder.value.length === 0 ? "FULANO DA SILVA" : cardHolder.value
+})
+//#endregion
+
+//#region IMask events
+//same logic in addEventListener
+securityCodeMasked.on("accept", () => {
+  updateSecurityCode(securityCodeMasked.value)
+})
+
+function updateSecurityCode(code) {
+  const ccSecurity = document.querySelector(".cc-security .value")
+
+  ccSecurity.innerText = code.length === 0 ? "123" : code
+}
+
+cardNumberMasked.on("accept", () => {
+  const cardType = cardNumberMasked.masked.currentMask.cardtype
+  setCardType(cardType)
+  updateCardNumber(cardNumberMasked.value)
+})
+
+function updateCardNumber(number) {
+  const ccNumber = document.querySelector(".cc-number")
+
+  ccNumber.innerText = number.length === 0 ? "1234 5678 9012 3456" : number
+}
+
+expirationDateMasked.on("accept", () => {
+  updateExpirationDate(expirationDateMasked.value)
+})
+
+function updateExpirationDate(date) {
+  const ccExpiration = document.querySelector(".cc-extra .cc-expiration .value")
+
+  ccExpiration.innerText = date.length === 0 ? "02/32" : date
+}
+
+//#endregion
+
+/**
+ * regras de cartão
+ *
+ * -------   visa   -------
+ * inicia com 4 seguido de mais 12 digitos
+ *
+ * exemplo:
+ * 4234 4234 4234 2344
+ *
+ * expressão regular: ^4\dP{0,15}
+ *
+ * -------mastercard-------
+ * inicia com 5, seguido de um digito entre 1 e 5, seguido de mais 2 digitos
+ * OU
+ * inicia com 22, seguido de um digito entre 2 e 9, seguido de mais um digito
+ * OU
+ * inicia com 2, seguido de um digito entre 3 e 7, seguido de mais dois digitos
+ * seguido de mais 12 digitos
+ *
+ * exemplo:
+ * 5353 5353 5353 5353
+ * 2323 2323 2323 2323
+ * 2234 3446 5545 5664
+ */
+
+// diversas expressões regulares para diferentes cartões
+//
+// const cardsDynamicMasks = [
+//   {
+//     mask: "0000 000000 00000",
+//     regex: /^3[47]\d{0,13}/,
+//     cardtype: "american express",
+//   },
+//   {
+//     mask: "0000 0000 0000 0000",
+//     regex: /^(?:6011|65\d{0,2}|64[4-9]\d?)\d{0,12}/,
+//     cardtype: "discover",
+//   },
+//   {
+//     mask: "0000 000000 0000",
+//     regex: /^3(?:0([0-5]|9)|[689]\d?)\d{0,11}/,
+//     cardtype: "diners",
+//   },
+//   {
+//     mask: "0000 0000 0000 0000",
+//     regex: /^(5[1-5]\d{0,2}|22[2-9]\d{0,1}|2[3-7]\d{0,2})\d{0,12}/,
+//     cardtype: "mastercard",
+//   },
+//   {
+//     mask: "0000 000000 00000",
+//     regex: /^(?:2131|1800)\d{0,11}/,
+//     cardtype: "jcb15",
+//   },
+//   {
+//     mask: "0000 0000 0000 0000",
+//     regex: /^(?:35\d{0,2})\d{0,12}/,
+//     cardtype: "jcb",
+//   },
+//   {
+//     mask: "0000 0000 0000 0000",
+//     regex: /^(?:5[0678]\d{0,2}|6304|67\d{0,2})\d{0,12}/,
+//     cardtype: "maestro",
+//   },
+//   {
+//     mask: "0000 0000 0000 0000",
+//     regex: /^4\d{0,15}/,
+//     cardtype: "visa",
+//   },
+//   {
+//     mask: "0000 0000 0000 0000",
+//     regex: /^62\d{0,14}/,
+//     cardtype: "unionpay",
+//   },
+// ]
